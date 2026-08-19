2979a4de414beea021bd234f622c4d827b401c2b
Aluno Vocacao nathandiasc <nathanaeldias845@gmail.com>
2026-08-19 11:35:18 -0300

    Upgrade

--- modulo07/modulo07_cis_ss_ativ01.py
+++ modulo07/modulo07_cis_ss_ativ01.py
@@ -3,4 +3,9 @@ import utilidades
 
 print(f"Soma: {utilidades.somar(10, 5)}")
 print(f"Subtração: {utilidades.subtrair(10, 5)}")
-print(f"Potência: {utilidades.potencia(2, 3)}")
+print(f"Multiplicação: {utilidades.multiplicar(6, 7)}")
+print(f"Divisão: {utilidades.dividir(4, 2)}")
+print(f"Divisão inteira: {utilidades.divisao_inteira(15,8)}")
+print(f"Resto divisão: {utilidades.resto_divisao(3, 0)}")
+print(f"Potenciação: {utilidades.potencia(10, 4)}")
+print(f"Calcular média: {utilidades.calcular_media(10, 8, 7,5)}")
